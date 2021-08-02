2d9dc534545388027b97b14bc87e63f19ac335e8
Prawee Wongsa <prawee@hotmail.com>
2021-08-02 18:35:29 +0700

    fething data and styles

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,6 +1,16 @@
 import Head from 'next/head'
+import styles from '../../styles/Ninjas.module.css'
 
-const Ninjas = () => {
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+
+    return {
+        props: { ninjas: data }
+    }
+}
+
+const Ninjas = (props) => {
     return (
         <>
             <Head>
@@ -9,6 +19,13 @@ const Ninjas = () => {
             </Head>
             <div>
                 <h1>All Ninjas</h1>
+                {props.ninjas.map(ninja => (
+                    <div key={ninja.id}>
+                        <a className={styles.single}>
+                            <h3>{ninja.name}</h3>
+                        </a>
+                    </div>
+                ))}
             </div>
         </>
     );
